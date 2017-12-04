364a1a73bd0a8804e722822b9666998916d624aa
Garrett Lance <31112155+Glance16540@users.noreply.github.com>
2017-12-04 11:59:23 -0500

    Add project files.

---
+++ Corebible/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Corebible.Startup))]
+namespace Corebible
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
